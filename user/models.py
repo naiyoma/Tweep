02b45a6a0bfa45825cf5e6619ada6a64abe97e02
Naiyoma <naiyoma@mesozi.com>
2021-07-07 02:30:32 +0300

    creating user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(
+        _('first name'), max_length=30,
+        blank=True, unique=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(_('active'), default=True)
+
+
+    REQUIRED_FIELDS = ()
+    USERNAME_FIELD = 'first_name'
+
+    def __str__(self):
+        return self.first_name
