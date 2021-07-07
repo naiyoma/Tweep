97640e4db4798bea639402ed28ceef735497e605
Naiyoma <naiyoma@mesozi.com>
2021-07-07 15:28:42 +0300

    psql as db

--- tweep/settings.py
+++ tweep/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'tweep.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tweepy',
+        'USER': 'lankas',
+        'PASSWORD': 'Naiyoma@lankas',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ tweepy.py
@@ -0,0 +1,17 @@
+
+import os
+import tweepy as tw
+import pandas as pd
+
+
+
+
+# # Authenticate to Twitter
+# auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+# auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+# # Create API object
+# api = tweepy.API(auth)
+
+# # Create a tweet
+# api.update_status("Hello Tweepy")
