436bba08e9c6b224549dbe6ecd0ee841f0ea1bdc
Naiyoma <naiyoma@mesozi.com>
2021-07-09 18:23:17 +0300

    env file

--- tweep/settings.py
+++ tweep/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +129,12 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+#Twitter
+env = environ.Env()
+environ.Env.read_env()
+consumer_secret_key = os.environ.get('API_SECRET_KEY')
+consumer_key = os.environ.get('API_KEY')
+bearer_token = os.environ.get('BEARER_TOKEN')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
--- tweet.py
+++ tweet.py
@@ -3,20 +3,3 @@ import os
 import tweepy as tw
 import pandas as pd
 from tweepy.auth import OAuthHandler
-
-
-# # Authenticate to Twitter
-# auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-# auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
-
-# # Create API object
-# api = tweepy.API(auth)
-
-# # Create a tweet
-# api.update_status("Hello Tweepy")
-
-API_SECRET_KEY = 'ZxXKs8gmfg6w61GIWkUhAfVV1HrI8S76oWbf6JMtk3jzJh9vRD'
-API_KEY = 'yNtiYh9eA99AzeDAvj3eg8QcQ'
-BEARER_TOKEN='AAAAAAAAAAAAAAAAAAAAABuURQEAAAAAXL4HCaEJyePzYh3o2M%2BGESxXUsU%3DCGFQJFo2YC91q356gh3AvmYAJhx61kwaqQO3mQJkTajwuYaOBi'
-ACCESS_TOKEN='1072560730451439616-owT3mm92S0Fc5sznvQ61i8WkmttWbB'
-ACESS_TOKEN_SECRET='2plWVwJjnTq2FaxZzfSCCatBPmLeAVfxsRgthbAFVcA3w'
