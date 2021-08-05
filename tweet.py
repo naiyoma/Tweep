11a31f128455b5e76853d2b8a5c139fa2e678045
Naiyoma <naiyoma@mesozi.com>
2021-08-05 17:28:40 +0300

    adding try except on api feed

--- tweet.py
+++ tweet.py
@@ -4,6 +4,8 @@ import tweepy
 import pandas
 from tweepy.auth import OAuthHandler
 
+f
+
 API_SECRET_KEY = "ZxXKs8gmfg6w61GIWkUhAfVV1HrI8S76oWbf6JMtk3jzJh9vRD"
 API_KEY = "yNtiYh9eA99AzeDAvj3eg8QcQ"
 # BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAABuURQEAAAAAXL4HCaEJyePzYh3o2M % 2BGESxXU
@@ -18,25 +20,123 @@ access_token_secret = ACCESS_TOKEN_SECRET
 # search for tweets function
 
 
-def search_tweet():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True)
-    username = 'jack'
-    count = 150
-    try:
-        # Creation of query method using parameters
-        import pdb
-        pdb.set_trace()
-        tweets = tweepy.Cursor(api.user_timeline, id=username).items(count)
+class UserListView(ListView):
+    model = User
 
-        # Pulling information from tweets iterable object
-        tweets_list = [[tweet.created_at, tweet.id, tweet.text]
-                       for tweet in tweets]
+    def get_context_data(self, **kwargs):
 
-        # Creation of dataframe from tweets list
-        # Add or remove columns as you remove tweet information
-        tweets_df = pandas.DataFrame(tweets_list)
-    except BaseException as e:
-        print('failed on_status,', str(e))
-        time.sleep(3)
+        context = super(UserListView, self).get_context_data(**kwargs)
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth, wait_on_rate_limit=True)
+        # last_tweet_id = False
+        # page_num = 1
+        # query = "BTC"
+        # data = get_df()
+        # cypto_query = f"#{query}"
+        # print(" ===== ", query, cypto_query)
+        # for page in tweepy.Cursor(
+        #     self.api.search,
+        #     q=cypto_query,
+        #     lang="en",
+        #     tweet_mode="extended",
+        #     count=200,
+        # ).pages():
+        #     print(" ...... new page", page_num)
+        #     page_num += 1
+
+        #     for item in page:
+        #         mined = {
+        #             "tweet_id": item.id,
+        #             "name": item.user.name,
+        #             "screen_name": item.user.screen_name,
+        #             "retweet_count": item.retweet_count,
+        #             "text": item.full_text,
+        #             "mined_at": datetime.datetime.now(),
+        #             "created_at": item.created_at,
+        #             "favourite_count": item.favorite_count,
+        #             "hashtags": item.entities["hashtags"],
+        #             "status_count": item.user.statuses_count,
+        #             "followers_count": item.user.followers_count,
+        #             "location": item.place,
+        #             "source_device": item.source,
+        #         }
+
+        #         try:
+        #             mined["retweet_text"] = item.retweeted_status.full_text
+        #         except:
+        #             mined["retweet_text"] = "None"
+
+        #         last_tweet_id = item.id
+        #         data = data.append(mined, ignore_index=True)
+
+        #     if page_num % 180 == 0:
+        #         date_label = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        #         print("....... outputting to csv", page_num, len(data))
+        #         data.to_csv(
+        #             f"{query}_{page_num}_{date_label}.csv", index=False)
+        #         print("  ..... resetting df")
+        #         data = get_df()
+
+        # date_label = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        # data.to_csv(f"{query}_{page_num}_{date_label}.csv", index=False)
+        # context['tweet_tweet_location'] = df.to_dict()['col1'][0]
+
+        context = "test user_data"
+
+        return context
+
+    template_name = 'home.html'
+
+    # search for Ethereum per minute
+
+    # tweets = tweepy.Cursor(
+    #     api.search,
+    #     q="#Ethereum",
+    #     lang="en",
+    #     start_time=start_time,
+    #     end_time=end_time,
+    #     tweet_mode="extended").items(100)
+    # all_tweets_et = [tweet.created_at.minute for tweet in tweets]
+    # current_time = datetime.datetime.now().minute
+    # filtered_tweets_et = [x for x in all_tweets_et if x == current_time
+    #                       or x == current_time - 1]
+
+    # # search for #Litecoin per  minute
+    # tweets = tweepy.Cursor(
+    #     api.search,
+    #     q="#Litecoin",
+    #     lang="en",
+    #     start_time=start_time,
+    #     end_time=end_time,
+    #     tweet_mode="extended").items(100)
+    # all_tweets_lt = [tweet.created_at.minute for tweet in tweets]
+    # current_time = datetime.datetime.now().minute
+    # filtered_tweets_lt = [x for x in all_tweets_lt if x == current_time
+    #                       or x == current_time - 1]
+
+    # # Search #XRP per minute
+    # tweets = tweepy.Cursor(
+    #     api.search,
+    #     q="#XRP",
+    #     lang="en",
+    #     start_time=start_time,
+    #     end_time=end_time,
+    #     tweet_mode="extended").items(100)
+    # all_tweets_bt = [tweet.created_at.minute for tweet in tweets]
+    # current_time = datetime.datetime.now().minute
+    # filtered_tweets_bt = [x for x in all_tweets_bt if x == current_time
+    #                       or x == current_time - 1]
+
+    # # Search for #DogeCoin per minute
+    # tweets = tweepy.Cursor(
+    #     api.search,
+    #     q="#DogeCoin",
+    #     lang="en",
+    #     start_time=start_time,
+    #     end_time=end_time,
+    #     tweet_mode="extended").items(100)
+    # all_tweets_dg = [tweet.created_at.minute for tweet in tweets]
+    # current_time = datetime.datetime.now().minute
+    # filtered_tweets_dg = [x for x in all_tweets_dg if x == current_time
+    #                       or x == current_time - 1]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,208 @@
 
-from django.views.generic import ListView, DetailView
+import os
+import tweepy
+import pandas as pd
 
+# from datetime import datetime, timedelta
+import datetime
+
+from tweepy.auth import OAuthHandler
+
+from django.views.generic import ListView, DetailView
 from .models import User
 
 # Create your views here.
 
+consumer_key = "yNtiYh9eA99AzeDAvj3eg8QcQ"
+consumer_secret = "ZxXKs8gmfg6w61GIWkUhAfVV1HrI8S76oWbf6JMtk3jzJh9vRD"
+access_token = "1072560730451439616-owT3mm92S0Fc5sznvQ61i8WkmttWbB"
+access_token_secret = "2plWVwJjnTq2FaxZzfSCCatBPmLeAVfxsRgthbAFVcA3w"
+
 
 class UserListView(ListView):
     model = User
+
+    def get_context_data(self, **kwargs):
+
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth, wait_on_rate_limit=True)
+        context = super(UserListView, self).get_context_data(**kwargs)
+
+        end_time = datetime.datetime.now().replace(microsecond=0)
+        start_time = (datetime.datetime.now().replace(
+            microsecond=0) - datetime.timedelta(minutes=1))
+        current_time = datetime.datetime.now().minute
+
+        # search for #Cardano per minute
+        tweets = tweepy.Cursor(
+            api.search,
+            q="#Cardano",
+            lang="en",
+            start_time=start_time,
+            end_time=end_time,
+            tweet_mode="extended").items(10)
+
+        et_tweets_dict = {}
+        for tweet in tweets:
+            et_tweets_dict[str(tweet.id)] = tweet.created_at.minute
+
+        for key, value in dict(et_tweets_dict).items():
+            if value < current_time - 2:
+                del et_tweets_dict[key]
+        ca_number = list(et_tweets_dict.values())
+
+        ca_favorite_count = []
+        ca_retweets_count = []
+        interactions_ca = sum(ca_favorite_count + ca_retweets_count)
+
+        for key, value in dict(et_tweets_dict).items():
+            try:
+                tweet = api.get_status(key)
+                ca_favorite_count.append(tweet.favorite_count)
+                ca_retweets_count.append(tweet.retweet_count)
+            except:
+                pass
+
+        context['ca_num'] = len(ca_number)
+        context["ca_interactions"] = interactions_ca
+
+        # search for #XRP per minute
+        tweets = tweepy.Cursor(
+            api.search,
+            q="#XRP",
+            lang="en",
+            start_time=start_time,
+            end_time=end_time,
+            tweet_mode="extended").items(10)
+
+        xrp_all_tweets_dict = {}
+        for tweet in tweets:
+            xrp_all_tweets_dict[str(tweet.id)] = tweet.created_at.minute
+
+        for key, value in dict(xrp_all_tweets_dict).items():
+            if value < current_time - 2:
+                del xrp_all_tweets_dict[key]
+        xrp_number = list(xrp_all_tweets_dict.values())
+
+        xrp_favorite_count = []
+        xrp_retweets_count = []
+        interactions_xrp = sum(xrp_favorite_count + xrp_retweets_count)
+
+        for key, value in dict(xrp_all_tweets_dict).items():
+            try:
+                tweet = api.get_status(key)
+                xrp_favorite_count.append(tweet.favorite_count)
+                xrp_retweets_count.append(tweet.retweet_count)
+            except:
+                pass
+
+        context['xrp_num'] = len(xrp_number)
+        context["xrp_interactions"] = interactions_xrp
+
+        # search for ##Litecoin per minute
+        tweets = tweepy.Cursor(
+            api.search,
+            q="#Litecoin",
+            lang="en",
+            start_time=start_time,
+            end_time=end_time,
+            tweet_mode="extended").items(10)
+
+        lt_all_tweets_dict = {}
+        for tweet in tweets:
+            lt_all_tweets_dict[str(tweet.id)] = tweet.created_at.minute
+
+        for key, value in dict(lt_all_tweets_dict).items():
+            if value < current_time - 2:
+                del lt_all_tweets_dict[key]
+        lt_number = list(lt_all_tweets_dict.values())
+
+        lt_favorite_count = []
+        lt_retweets_count = []
+        interactions_lt = sum(lt_favorite_count + lt_retweets_count)
+
+        for key, value in dict(lt_all_tweets_dict).items():
+            try:
+                tweet = api.get_status(key)
+                lt_favorite_count.append(tweet.favorite_count)
+                lt_retweets_count.append(tweet.retweet_count)
+            except:
+                pass
+
+        context['lt_num'] = len(lt_number)
+        context["lt_interactions"] = interactions_lt
+
+        # search for #DogeCoin per minute
+        tweets = tweepy.Cursor(
+            api.search,
+            q="#DogeCoin",
+            lang="en",
+            start_time=start_time,
+            end_time=end_time,
+            tweet_mode="extended").items(10)
+
+        dg_all_tweets_dict = {}
+        for tweet in tweets:
+            dg_all_tweets_dict[str(tweet.id)] = tweet.created_at.minute
+
+        for key, value in dict(dg_all_tweets_dict).items():
+            if value < current_time - 2:
+                del dg_all_tweets_dict[key]
+        dg_number = list(dg_all_tweets_dict.values())
+
+        dg_favorite_count = []
+        dg_retweets_count = []
+        interactions_dg = sum(dg_favorite_count + dg_retweets_count)
+
+        for key, value in dict(dg_all_tweets_dict).items():
+            try:
+                tweet = api.get_status(key)
+                dg_favorite_count.append(tweet.favorite_count)
+                dg_retweets_count.append(tweet.retweet_count)
+            except:
+                pass
+
+        context['dg_num'] = len(dg_number)
+        context["dg_interactions"] = interactions_dg
+
+        # search for ##Ethereum per minute
+        tweets = tweepy.Cursor(
+            api.search,
+            q="#Ethereum",
+            lang="en",
+            start_time=start_time,
+            end_time=end_time,
+            tweet_mode="extended").items(50)
+
+        current_time = datetime.datetime.now().minute
+
+        et_all_tweets_dict = {}
+        for tweet in tweets:
+            et_all_tweets_dict[str(tweet.id)] = tweet.created_at.minute
+
+        for key, value in dict(et_all_tweets_dict).items():
+            if value < current_time - 2:
+                del et_all_tweets_dict[key]
+        et_number = list(et_all_tweets_dict.values())
+
+        et_favorite_count = []
+        et_retweets_count = []
+        interactions_et = sum(et_favorite_count + et_retweets_count)
+
+        for key, value in dict(et_all_tweets_dict).items():
+            try:
+                tweet = api.get_status(key)
+                et_favorite_count.append(tweet.favorite_count)
+                et_retweets_count.append(tweet.retweet_count)
+            except:
+                pass
+
+        context['et_num'] = len(et_number)
+        context["et_interactions"] = interactions_et
+
+        return context
+
     template_name = 'home.html'
 
 
