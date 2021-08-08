3e4fce236168785234c9f16712974f2597a9795f
Naiyoma <naiyoma@mesozi.com>
2021-08-09 00:17:47 +0300

    adding ca interactions

--- user/views.py
+++ user/views.py
@@ -64,7 +64,7 @@ class UserListView(ListView):
                 ca_retweets_count.append(tweet.retweet_count)
             except:
                 pass
-
+        # adding interactions per second to context data.
         context['ca_num'] = len(ca_number)
         context["ca_interactions"] = interactions_ca
 
